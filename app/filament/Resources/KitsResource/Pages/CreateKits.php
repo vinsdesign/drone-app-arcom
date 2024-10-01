dcf8ef3250e790d56acff1d9b2255453d69090cc
evinsdesign <kelvinsaputra024@gmail.com>
2024-10-01 21:01:39 +0800

    feat: direct hal ke view table kit (index)

--- app/filament/Resources/KitsResource/Pages/CreateKits.php
+++ app/filament/Resources/KitsResource/Pages/CreateKits.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateKits extends CreateRecord
 {
     protected static string $resource = KitsResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/filament/Resources/KitsResource/Pages/EditKits.php
+++ app/filament/Resources/KitsResource/Pages/EditKits.php
@@ -17,4 +17,8 @@ class EditKits extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
